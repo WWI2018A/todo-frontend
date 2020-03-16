bce057e00003a3d8b0c5c67dfff06fdaad242f3e
Vithilaka <55878747+Vithilaka@users.noreply.github.com>
2020-03-16 12:23:18 +0100

    recipe

---
+++ pages/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <section>
+    <h1>Willkommen auf meiner Recipe Seite</h1>
+    <p>Tutorial: https://www.youtube.com/watch?v=nteDXuqBfn0</p>
+    <p>Mit 'npm run dev' startet man den Server </p>
+  </section>
+</template>
---
+++ pages/recipes/index.vue
@@ -0,0 +1,59 @@
+<template>
+    <section class="recipes">
+        <Recipe 
+            v-for="r in recipes"
+            :key="r.id"
+            :thumbnail="r.thumbnail"
+            :title="r.title"
+            :id="r.id"
+            :previewText="r.previewText" />
+    </section>
+</template>
+
+<script>
+import Recipe from '@/components/Recipe'
+export default {
+    components: {
+        Recipe
+    },
+    asyncData(){
+        //hier kommt der HTTP Aufruf hin
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+            resolve({
+                recipes:[
+                    {
+                        id:"1",
+                        title:"Delicious Pizza",
+                        previewText: "Awesome Pizza!",
+                        thumbnail:"https://images.pexels.com/photos/2232/vegetables-italian-pizza-restaurant.jpg?cs=srgb&dl=gemuse-italienisch-pizza-restaurant-2232.jpg&fm=jpg"
+                    },
+                    {
+                        id:"2",
+                        title:"Soup",
+                        previewText: "Very tasty indeed!",
+                        thumbnail:"https://cdn.pixabay.com/photo/2017/04/04/00/36/japanese-food-2199962_960_720.jpg"
+                    },
+                    {
+                        id:"3",
+                        title:"Fantastic Burger",
+                        previewText: "Yumiee!",
+                        thumbnail:"https://cdn.pixabay.com/photo/2018/03/06/16/22/burgers-3203841_960_720.jpg"
+                    }
+                ]
+            })
+            },1000)
+        })
+    }
+}
+</script>
+
+<style scoped>
+.recipes{
+    display: flex;
+    flex-flow: row wrap;
+    justify-content: center;
+    align-items:center;
+}
+
+</style>
