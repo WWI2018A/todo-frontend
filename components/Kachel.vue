8db4587714a9c108b446acaceaae4c2d75c795d6
Johannes Wiest <johannes.wiest@muenchen-mail.de>
2020-04-18 19:07:00 +0200

    link undefinded fix

--- components/Kachel.vue
+++ components/Kachel.vue
@@ -1,17 +1,17 @@
 <template>
- <nuxt-link :to="'/todoLists/'+id">
+  <nuxt-link :to="{path: '/todoLists/' + todoList.id, props: {todoList: this.todoList}}">
     <article class="todoLists">
-            <div class="thumbnail"></div>
-                <h3 class="name"> {{ todoList.name }} </h3>
+      <div class="thumbnail"></div>
+      <h3 class="name"> {{ todoList.name }} </h3>
 
-        </article>
- </nuxt-link>
+    </article>
+  </nuxt-link>
 </template>
 
 
 <script>
-export default {
-  props: {
+  export default {
+    props: {
       todoList: {
         id: String,
         createdDate: Date,
@@ -19,47 +19,47 @@ export default {
         userId: String,
         name: String,
       }
-}
-}
+    }
+  }
 </script>
 
 <style scoped>
 
-a{
+  a {
     text-decoration: none;
     text-align: center;
     font-size: 18px;
     font-family: Didot;
-    color:rgb(0, 0, 0);
+    color: rgb(0, 0, 0);
     margin: 0px;
-}
+  }
 
-.name {
+  .name {
     background-color: rgb(255, 255, 255);
     text-align: center;
     padding: 0px;
-}
+  }
 
-.todoLists {
+  .todoLists {
     margin: 60px 30px 5px 5px;
-    -webkit-box-shadow: 10px 10px 18px 4px rgba(92,92,92,0.78);
-    -moz-box-shadow: 10px 10px 18px 4px rgba(92,92,92,0.78);
-    box-shadow: 10px 10px 18px 4px rgba(92,92,92,0.78);
-}
+    -webkit-box-shadow: 10px 10px 18px 4px rgba(92, 92, 92, 0.78);
+    -moz-box-shadow: 10px 10px 18px 4px rgba(92, 92, 92, 0.78);
+    box-shadow: 10px 10px 18px 4px rgba(92, 92, 92, 0.78);
+  }
 
-.kachel{
+  .kachel {
     box-sizing: border-box;
     width: 200px;
     padding: 10px;
     border: 1px solid #ccc;
     margin: 0px;
-}
+  }
 
-.thumbnail{
+  .thumbnail {
     background-position: center;
     background-size: cover;
-    width:100%;
+    width: 100%;
     height: 300px;
     margin: 0px;
-}
+  }
 </style>
